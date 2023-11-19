c3115696b9d26bb68730274406a399bc4c794249
Emiel <92210989+Bethamil@users.noreply.github.com>
2023-11-19 02:05:33 +0100

    removed motions from about, since it wouldnt work on mobile

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,11 +11,10 @@ const ServiceCard = ({ index, title, icon }) => {
   return (
     <Tilt className="xs:w-[250px] w-full">
       <motion.div
-        variants={fadeIn("right", "spring", index * 0.5, 0.75)}
         className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
       >
         <div
-          options={{ max: 25, scale: 1, speed: 450 }}
+          // options={{ max: 25, scale: 1, speed: 450 }}
           className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
         >
           <img src={icon} alt={title} className='w-[60px] h-[60px] object-contain' />
@@ -30,14 +29,13 @@ const ServiceCard = ({ index, title, icon }) => {
 const About = () => {
   return (
     <>
-      <motion.div variants={textVariant()}>
-        <p className={styles.sectionSubText}>Introduction</p>
+      <div >
+      <p className={styles.sectionSubText}>Introduction</p>
         <h2 className={styles.sectionHeadText}>Overview</h2>
-      </motion.div>
-      <motion.p
-        variants={fadeIn("", "", 0.1, 1)}
+      </div>
+      <p
         className="mt-4 text-secondary text-[17px] max-w-3xl leadin-[30px]"
-      >{aboutMe}</motion.p>
+      >{aboutMe}</p>
 
       <div className='mt-20 flex flex-wrap gap-10'>
         {services.map((service, index) => (
